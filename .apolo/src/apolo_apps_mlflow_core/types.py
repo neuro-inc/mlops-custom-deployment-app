76bb71ac1afe97b7239ae6c3a741d6e4e13b011b
Yevhenii Semendiak <semendyak@gmail.com>
2025-04-10 15:35:48 +0300

    MLO-198: Add custom deployment, MLFlow applications definitions

---
+++ .apolo/src/apolo_apps_mlflow_core/types.py
@@ -0,0 +1,8 @@
+from apolo_app_types.protocols.mlflow import (
+    # later, this class definitions will move here,
+    # for now, leaving it in the origin repo since we are actively updating it
+    MLFlowAppInputs,  # noqa: F401
+    # apart from it, this definition might still be in shared repo,
+    # since it's used by other apps
+    MLFlowAppOutputs,  # noqa: F401
+)
