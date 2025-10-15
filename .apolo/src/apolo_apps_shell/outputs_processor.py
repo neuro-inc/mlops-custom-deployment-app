11435b47c367490994f927eb01948c392943c20d
jordyantunes <jordy.antunes@hotmail.com>
2025-10-15 13:54:58 -0300

    Refactor outputs processor to use correct output functions

--- .apolo/src/apolo_apps_shell/outputs_processor.py
+++ .apolo/src/apolo_apps_shell/outputs_processor.py
@@ -1,7 +1,7 @@
 import typing as t
 
 from apolo_app_types.outputs.base import BaseAppOutputsProcessor
-from apolo_app_types.outputs.custom_deployment import get_custom_deployment_outputs
+from apolo_app_types.outputs.shell import get_shell_outputs
 
 from .app_types import ShellAppOutputs
 
@@ -12,5 +12,5 @@ class ShellAppOutputProcessor(BaseAppOutputsProcessor[ShellAppOutputs]):
         helm_values: dict[str, t.Any],
         app_instance_id: str,
     ) -> ShellAppOutputs:
-        res = await get_custom_deployment_outputs(helm_values, app_instance_id)
+        res = await get_shell_outputs(helm_values, app_instance_id)
         return ShellAppOutputs.model_validate(res)
