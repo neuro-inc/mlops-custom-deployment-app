dddcd99c4a460e4b195d03a21121f1caf88dc6a9
Jordy Jackson Antunes da Rocha <jordy.antunes@hotmail.com>
2025-11-18 11:52:24 -0300

    Added OpenWebUI versioned app (#113)
    * Added OpenWebUI versioned app
    
    * Moved OpenWebUI outputs processor to this repo
    
    * Moved OpenWebUI input protocols definition to this repo

---
+++ .apolo/src/apolo_apps_openwebui/__init__.py
@@ -0,0 +1,15 @@
+from apolo_apps_openwebui.inputs_processor import OpenWebUIInputsProcessor
+from apolo_apps_openwebui.outputs_processor import OpenWebUIOutputProcessor
+from apolo_apps_openwebui.types import OpenWebUIAppInputs, OpenWebUIAppOutputs
+
+
+APOLO_APP_TYPE = "openwebui"
+
+
+__all__ = [
+    "APOLO_APP_TYPE",
+    "OpenWebUIInputsProcessor",
+    "OpenWebUIOutputProcessor",
+    "OpenWebUIAppInputs",
+    "OpenWebUIAppOutputs",
+]
