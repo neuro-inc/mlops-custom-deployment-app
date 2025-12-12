f4cf45a40687784a0c6c818de1481463c8d7a2e8
Jordy Antunes <jordy.antunes@hotmail.com>
2025-12-12 09:03:53 -0300

    Removed output type import from app-types

--- .apolo/src/apolo_apps_fooocus/types.py
+++ .apolo/src/apolo_apps_fooocus/types.py
@@ -1,14 +1,18 @@
 from apolo_app_types.protocols.common import (
     AppInputs,
+    AppOutputs,
     IngressHttp,
     Preset,
 )
-from apolo_app_types.protocols.fooocus import FooocusAppOutputs
 
 
 class FooocusAppInputs(AppInputs):
     preset: Preset
     ingress_http: IngressHttp
+
+
+class FooocusAppOutputs(AppOutputs):
+    pass
 
 
 __all__ = ["FooocusAppInputs", "FooocusAppOutputs"]
