6481e43dab90700904eb08ba91e39df04cb8b122
Vad Tymoshyk <vt781309@gmail.com>
2026-03-16 19:03:11 -0400

    Fix issues with VSCode

--- .apolo/src/apolo_apps_fooocus/types.py
+++ .apolo/src/apolo_apps_fooocus/types.py
@@ -3,8 +3,9 @@ from pydantic import Field
 from apolo_app_types.protocols.common import (
     AppInputs,
     AppOutputs,
-    IngressHttp,
     Preset,
+)
+from apolo_app_types.protocols.common.schema_extra import (
     SchemaExtraMetadata,
 )
 
@@ -13,13 +14,13 @@ class FooocusAppInputs(AppInputs):
     preset: Preset = Field(
         ...,
         json_schema_extra=SchemaExtraMetadata(
-            title="Foocus preset",
+            title="Fooocus preset",
             description="Select the resource preset used for the "
-            "Foocus instance. "
-            "Minimal resources: 4 CPU cores, 8 GiB memory, 1 GPU with 4 GiB memory.",
+            "Fooocus instance. "
+            "Minimal resources: 4 CPU cores, 8 GiB memory, "
+            "1 GPU with 4 GiB memory.",
         ).as_json_schema_extra(),
     )
-    ingress_http: IngressHttp
 
 
 class FooocusAppOutputs(AppOutputs):
--- .apolo/src/apolo_apps_vscode/types.py
+++ .apolo/src/apolo_apps_vscode/types.py
@@ -1,3 +1,13 @@
+from pydantic import Field
+
+from apolo_app_types.protocols.common.schema_extra import (
+    SchemaExtraMetadata,
+)
+from apolo_app_types.protocols.custom_deployment import (
+    CustomDeploymentInputs,
+    CustomDeploymentOutputs,
+    Preset,
+)
 from apolo_app_types.protocols.vscode import (
     # later, this class definitions will move here,
     # for now, leaving it in the origin repo since we are actively updating it
@@ -6,3 +16,19 @@ from apolo_app_types.protocols.vscode import (
     # since it's used by other apps
     VSCodeAppOutputs,  # noqa: F401
 )
+
+
+class VSCodeInputs(CustomDeploymentInputs):
+    preset: Preset = Field(
+        ...,
+        json_schema_extra=SchemaExtraMetadata(
+            title="VSCode preset",
+            description="Select the resource preset used for the instance. "
+            "Minimal resources depends on your application needs, "
+            "but a good starting point is 0.5 CPU cores and 512 MiB memory.",
+        ).as_json_schema_extra(),
+    )
+
+
+class VSCodeOutputs(CustomDeploymentOutputs):
+    pass
