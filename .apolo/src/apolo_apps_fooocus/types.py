714bc932f8b07282596fdf7b140d44f0f190f28d
Vad Tymoshyk <vt781309@gmail.com>
2026-03-16 12:15:23 -0400

    Fix issues with foocus; jupyter

--- .apolo/src/apolo_apps_fooocus/types.py
+++ .apolo/src/apolo_apps_fooocus/types.py
@@ -1,13 +1,24 @@
+from pydantic import Field
+
 from apolo_app_types.protocols.common import (
     AppInputs,
     AppOutputs,
     IngressHttp,
     Preset,
+    SchemaExtraMetadata,
 )
 
 
 class FooocusAppInputs(AppInputs):
-    preset: Preset
+    preset: Preset = Field(
+        ...,
+        json_schema_extra=SchemaExtraMetadata(
+            title="Foocus preset",
+            description="Select the resource preset used for the "
+            "Foocus instance. "
+            "Minimal resources: 4 CPU cores, 8 GiB memory, 1 GPU with 4 GiB memory.",
+        ).as_json_schema_extra(),
+    )
     ingress_http: IngressHttp
 
 
